482ce2332273fb5efc400cd0ff2876c91da203cb
Dirk Eddelbuettel <edd@debian.org>
2026-04-26 11:19:36 -0500

    Added Debian status

--- code.R
+++ code.R
@@ -10,7 +10,8 @@ p[, .(Package, Version)]
 fwrite(p[, .(Package, Version)], "packages.csv")
 
 
-## switch to code.sh and install some packages, then
+
+## switch to code.sh and install some packages in Ubuntu 26.04, then
 pkgs <- fread("packages.csv")
 pkgs[, Binary := paste0("r-cran-", tolower(Package)), by="Package"]
 pkpg[, Loads_26.04 := suppressMessages(require(Package, character.only=TRUE, quietly=TRUE)), by="Package"]
@@ -19,3 +20,21 @@ fwrite(pkgs, "packages.csv")
 
 ## after installation of rlang, packages igraph and sparsevtrs load
 ## after installation of vctrs, packages leidenAlg, archive and arrow also load
+
+
+
+## now same in Debian unstable
+## binary r-cran-data.table in unstable already works: 'apt install -t unstable r-cran-data.table'
+library("data.table")
+pkgs <- fread("packages.csv")
+
+## determine which packages are in Debian, and get their versions
+pkgs[, Debian := system(paste("apt-cache show", Binary), intern=FALSE, ignore.stderr=TRUE, ignore.stdout=TRUE) == 0, by=Binary]
+pkgs[Debian == TRUE, Unstable_Version := as.character(system(paste("apt-cache show", Binary, "| awk '/Version/ {print $2}' | head -1"), intern=TRUE)), by=Binary]
+
+## install them
+ign <- pkgs[Debian == TRUE, system(paste("apt-get -y -t unstable --no-install-recommends install", Binary)), by=Binary]
+## and try loading
+pkps[Debian == TRUE, Loads_Unstable := suppressMessages(require(Package, character.only=TRUE, quietly=TRUE)), by="Package"]
+
+fwrite(pkgs, "packages.csv")
